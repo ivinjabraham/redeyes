4adb50987d2fa507292f8ab7f4b1982b2ce197d2
LiquidFlare <ivinjoelabraham@gmail.com>
2023-02-06 21:06:29 +0530

    ADD: Implement user input through GUI, Fix bug with next & previous buttons.

--- main.py
+++ main.py
@@ -1,49 +1,82 @@
-from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel
+from PyQt5.QtWidgets import QMainWindow, QApplication, QPushButton, QLabel, QComboBox, QDateEdit
 from PyQt5 import uic
 from PyQt5.QtGui import QPixmap
+from PyQt5.QtCore import Qt
 import sys
 import requests
+import datetime
+import urllib.request
 import os
-import shutil
+from threading import *
 
 class UI(QMainWindow):
     
-    current_image = 0 
+    current_image = 0 # Basically filename without EXT
+    last_image = -1
 
     def __init__(self):
         super(UI, self).__init__()
 
+        # Loads UI from file
         uic.loadUi('base.ui', self)
 
+        self.setWindowTitle("Martian Chronicles")
+
+        # Declares buttons and labels
+        self.dropdown = self.findChild(QComboBox, "comboBox")
         self.next_button = self.findChild(QPushButton, "pushButton")
         self.previous_button = self.findChild(QPushButton, "pushButton_2")
+        self.email = self.findChild(QPushButton, "pushButton_3")
         self.fetch_button = self.findChild(QPushButton, "pushButton_4")
+        self.date = self.findChild(QDateEdit, "dateEdit")
 
         self.image = self.findChild(QLabel, "label")
 
+        self.pixmap = QPixmap(f"default.png")
+        self.image.setPixmap(self.pixmap)
+        self.image.setAlignment(Qt.AlignCenter)
+
+        # Link buttons with onClick functions
         self.next_button.clicked.connect(self.next)
         self.previous_button.clicked.connect(self.previous)
         self.fetch_button.clicked.connect(self.fetch)
+        
         self.show()
+    
+    # def pog(self):
+        # print("oaksd")
 
+    # Go to next image 
     def next(self):
         self.pixmap = QPixmap(f"images/{self.current_image}.png")
-        self.current_image += 1 #!TODO: Check if > max and loop back if true 
+        if os.path.isfile(f"images/{self.current_image + 1}.png"):
+            self.current_image += 1
+        else:
+            self.current_image = 0 
         self.image.setPixmap(self.pixmap)
     
+    # Go to previous image
     def previous(self):
         self.pixmap = QPixmap(f"images/{self.current_image}.png")
-        self.current_image -= 1 #!TODO: Check if < 0 and loop back if true 
+        if os.path.isfile(f"images/{self.current_image - 1}.png"):
+            self.current_image -= 1
+        else:
+            self.current_image = self.last_image
+ 
         self.image.setPixmap(self.pixmap)
 
+    # Fetch images from API
     def fetch(self):
-
+            
+        # ENV variable
         API_KEY = os.getenv("NASAKEY")
 
+        # Construct request
         base_url = "https://api.nasa.gov/mars-photos/api/v1/rovers/"
-        rover = input("ASdsA: ? ") #TODO: Get from APP
-        url = base_url + rover + "/photos?"
-        date = input("DATE: ") #TODO: Get from APP
+        rover = self.dropdown.currentText()
+        url = base_url + rover.lower() + "/photos?"
+        datetimeDate = self.date.date().toPyDate()
+        date = datetimeDate.strftime('%Y-%m-%d')
 
         parameters = {
             "earth_date": date,
@@ -53,25 +86,35 @@ class UI(QMainWindow):
         # curiosity works : 2015-6-3
         # while spirt doesn't  
 
+        # Send GET request and get list of photo elements 
         response = requests.get(url, params=parameters)
         photo_list = response.json()['photos'] #! Handle non positive responses 
-        photos = []
 
+        # Handles days where there are no photos taken
         if response.json()['photos'] == []:
             #TODO handle no photos
             pass 
         
+        # GET all photos and save it with whole number names.
         for i in range(len(photo_list)):
-            photos.append(photo_list[i]['img_src'])
+            if i == 10:
+                break
 
-        for idx, image_url in enumerate(photos):
-            res = requests.get(image_url, allow_redirects=True)
-            if res.status_code == 200:
-                with open(f"images/{idx}.png", "wb") as file:
-                    file.write(res.content)
-                print(f"{idx} done") #TODO: Display to user
+            res = urllib.request.urlopen(photo_list[i]['img_src'])
+            
+            # Succesful response
+            if res.getcode() == 200:
+                with open(f"images/{i}.png", "wb") as file:
+                    file.write(res.read())
+                    print(f"{i} done") #TODO: Display to user
+                self.last_image += 1
+            
+            # Failed GET
             else:
-                print(f"{idx} skipped")
+                print(f"{i} skipped")
+        
+        print("hehe yes")
+
 
 app = QApplication(sys.argv)
 UIWindow = UI()
