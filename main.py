b9110163e3b3db3d98a3bd48ea66b8a8eafc4384
Ivin <ivinjabraham+work@gmail.com>
2024-07-31 18:37:34 +0530

    style: add blanklines for clarity

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
-import sys
 from PyQt5.QtWidgets import QApplication
+import sys
+
 from controllers.main_controller import MainController
+
 
 def main():
     app = QApplication(sys.argv)
     controller = MainController()
     controller.show_api_key_form()
     sys.exit(app.exec_())
+
 
 if __name__ == '__main__':
     main()
