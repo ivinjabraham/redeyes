0a6fc79797a5044c92907555e3ee31a648ebd50a
Ivin <ivinjabraham+work@gmail.com>
2024-07-31 18:40:50 +0530

    style: add blanklines for clarity

--- controllers/main_controller.py
+++ controllers/main_controller.py
@@ -1,6 +1,8 @@
 from PyQt5.QtWidgets import QMessageBox
+
 from views.main_window import MainWindow
 from controllers.api_key_controller import ApiKeyController
+
 
 class MainController:
     def __init__(self):
@@ -16,5 +18,6 @@ class MainController:
         self.api_key_controller.close_form()
 
     def show_error(self):
-        QMessageBox.critical(self.api_key_controller.api_key_form, 'Error', 'Invalid API Key. Please try again.')
+        QMessageBox.critical(self.api_key_controller.api_key_form, 'Error',
+                             'Invalid API Key. Please try again.')
         self.api_key_controller.api_key_form.api_key_input.setText('')
